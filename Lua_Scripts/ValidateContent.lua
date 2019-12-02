7df06d6d62bb7e1262b042ee492392570aeaa5f2
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-12-02 23:41:51 +0200

    Write the basic structure of ValidateContent.lua

---
+++ Lua_Scripts/ValidateContent.lua
@@ -0,0 +1,56 @@
+--This script validates that all the JSON files in the Engine-Documentation directory follow the data structure defined in the specification.
+--Specification Implemented: 2019-11-17-Specification.md
+
+--Code author: Rami Sabbagh (RamiLego4Game)
+
+--Extend the package path so it search for the modules in the special directory.
+package.path = "./Lua_Scripts/Modules/?.lua;./Lua_Scripts/Modules/?/init.lua;"..package.path
+
+local startClock = os.clock()
+
+--Load ANSI module
+if not pcall(require,"ANSI") then print("\27[0;1;31mCould not load the ANSI module, please make sure that the script is executed with the repository being the working directory!\27[0;1;37m") os.exit(1) return end
+local ANSI = require("ANSI")
+
+--== Shared functions ==--
+
+local function fail(...)
+	local reason = {...}
+	for k,v in pairs(reason) do reason[k] = tostring(v) end
+	reason = table.concat(reason, " ")
+	ANSI.setGraphicsMode(0, 1, 31) --Red output
+	print(reason)
+	ANSI.setGraphicsMode(0, 1, 37) --White output
+	os.exit(1)
+end
+
+--== Load external modules ==--
+
+if not pcall(require,"lfs") then fail("Could not load luafilesystem, please make sure it's installed using luarocks first!") end
+local lfs = require("lfs")
+
+--== Validate the files content ==--
+
+ANSI.setGraphicsMode(0, 37) --Light grey output
+print("")
+print("Documentation files content validation script (ValidateContent.lua) by Rami Sabbagh (RamiLego4Game)")
+print("Using specification 2019-11-17")
+print("")
+
+ANSI.setGraphicsMode(0, 1, 34) --Blue output
+print("Validating the files content.")
+print("")
+
+--== Validate Engine_Documentation ==--
+
+--== The end of the script ==--
+
+local endClock = os.clock()
+local executionTime = endClock - startClock
+
+ANSI.setGraphicsMode(0, 1, 32) --Green output
+print("")
+print("The documentation files content has been validated successfully in "..executionTime.."s.")
+print("")
+
+ANSI.setGraphicsMode(0, 1, 37) --White output
