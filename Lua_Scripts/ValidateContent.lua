86566af3e0b6c18fb7bf3d9132d8bf4d20dcd277
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-12-27 00:54:09 +0200

    Implement type field validation method

--- Lua_Scripts/ValidateContent.lua
+++ Lua_Scripts/ValidateContent.lua
@@ -45,6 +45,54 @@ print("")
 
 --== Validate Engine_Documentation ==--
 
+local simpleTypes = {"number", "string", "boolean", "nil", "table", "userdata", "function"}
+for k,v in ipairs(simpleTypes) do simpleTypes[v] = k end
+
+--Returns true if the type was valid, false otherwise, with reason followed
+local function validateType(vtype)
+	if type(vtype) == "string" then
+		--Check if it's the any type
+		if vtype == "any" then return true end
+
+		--Check if it's a simple type
+		if simpleTypes[vtype] then return true end
+
+		--Otherwise it's an invalid type
+		return false, "Invalid simple type: "..vtype.."!" end
+
+	elseif type(vtype) == "table" then
+		local l1 = #vtype
+		if l1 == 0 then return false, "The type as a table of types must not be empty!" end
+		for k1, v1 in pairs(vtype) do
+			if type(k1) ~= "number" or k1 > l1 or k1 < 1 then return false, "The type as a table of types can be only an array of continuous values!" end
+
+			if type(v1) == "string" then
+				if v1 == "any" then return false, "The \"any\" type can't be used in a table of types!" end
+				if not simpleTypes[v1] then return false, "Invalid simple type: "..vtype.."!" end
+
+			elseif type(v1) == "table" then --Complex type
+				--Make sure that it's a valid array
+				local l2 = #v1
+				if l2 == 0 then return false, "The complex type in the table of types at index #"..k1.." must not be an empty array!" end
+				for k2, v2 in pairs(v1) do
+					if type(k2) ~= "number" or k2 > l2 or k2 < 1 then return false, "The complex type in the table of types at index #"..k1..": can be only an array of continuous values!" end
+					if type(v2) ~= "string" then return false, "The complex type in the table of types at index #"..k1..": has a non-string value!" end
+				end
+
+				--Make sure that it's a valid path
+				local complexPath = table.concat(v1, "/") .. "/" .. v1[l2] .. ".json"
+				if l2 < 2 or v1[l1-1] ~= "objects" or not isFile(complexPath) then return false, "Invalid object path for the complex type in the table of types at index #"..k1.."!" end
+			else
+				return false, "Invalid type of a type value in the types table at index #"..k1..": "..type(v1)..", it can be only a string or a table!"
+			end
+		end
+
+		return true --Validated successfully
+	else
+		return false, "Invalid type of the type value: "..type(vtype)..", it can be only a string or a table!"
+	end
+end
+
 --== The end of the script ==--
 
 local endClock = os.clock()
